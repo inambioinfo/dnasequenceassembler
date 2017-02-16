b5ee1168b9807e2146aed9330e7dd611081091d2
Paola Castro <paolacastropaolacastro@gmail.com>
2017-02-15 22:29:36 -0800

    make python package and add data files

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # dnasequencealigner
+
+Example input:
+
+>Frag_56
+ATTAGACCTG
+>Frag_57
+CCTGCCGGAA
+>Frag_58
+AGACCTGCCG
+>Frag_59
+GCCGGAATAC
+
+Example output:
+
+ATTAGACCTGCCGGAATAC
+
+FASTA PARSER
+
+make sure all the sequences are FASTA
+FASTA criteria:
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='dnasequencealigner',
+    version='0.0.0',
+    packages=['dnasequencealigner',],
+    license='figures out a unique DNA sequence from overlapping DNA sequences',
+    long_description=open('README.txt').read(),
+)
