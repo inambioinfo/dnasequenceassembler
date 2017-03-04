8a22068e0aa5fc6ca76e7cdaa90a97cea4926323
Paola Castro <paolacastropaolacastro@gmail.com>
2017-03-03 19:36:40 -0800

    Take2 (#1)
    * more testcases
    
    * find all the bottoms
    
    * take_2
    
    * fix printing statements

--- dnasequenceassembler/assembler.py
+++ dnasequenceassembler/assembler.py
@@ -1,146 +1,128 @@
+import copy
+import random
 
 class Assembler():
     """
     input: list of strings. each string is a sequences
     output: one string
     """
-    MATCHING_TOP_KEY = 'match_top'
-    MATCHING_BOTTOM_KEY = 'match_bottom'
 
     def __init__(self,sequences):
+        self.bottoms_dict_key = 'bottoms'
+        self.indices_dict_key = 'indices'
         self.sequences = sequences
         self.seqs = {}
         for idx, sequence in enumerate(self.sequences):
             self.seqs[idx] = list(sequence)
 
 
+    def fragment_matcher(self, top_seq, bottom_seq):
+        '''
+        '''
+        top_len = len(top_seq)
+        bottom_len = len(bottom_seq)
+        min_match_length = (max(top_len, bottom_len) / 2) + 1
+        sequences_match = False
+        bottom_string = ''.join(bottom_seq[0:min_match_length])
+        x = 0
+        x_stop = x + min_match_length
+        while x_stop <= top_len:
+            top_string = top_seq[x: x_stop]
+            if ''.join(top_string) == bottom_string:
+                sequences_match = True
+                return x
+            x +=1
+            x_stop +=1
+
+        return False
+
+
     def _find_matching_pairs(self):
         '''
-        finds pairs of sequences that overlap with each other
-
-        creates the following variables:
-            top_seq_dict
-            bottom_seq_dict
-            top_ranges_dict
-            bottom_ranges_dict
         '''
-        top_seq_dict = {}
-        bottom_seq_dict = {}
-        top_ranges_dict = {}
-        bottom_ranges_dict = {}
+        map_top_bottom = {}
+        map_bottom_top = {}
 
         num_sequences = len(self.sequences)
-        match_id = 0
 
-        for top_index in range(0, num_sequences):
-            # print 'top_index', top_index
-            for bottom_index in range(0, num_sequences):
-                if top_index != bottom_index:
-                    top_seq = self.seqs[top_index]
-                    bottom_seq = self.seqs[bottom_index]
-                    top_len = len(top_seq)
-                    bottom_len = len(bottom_seq)
-                    self.min_match_length = (max(top_len, bottom_len) / 2) + 1
+        for ID_top in range(0, num_sequences):
+            for ID_bottom in range(0, num_sequences):
+                if ID_top != ID_bottom:
+                    top_seq = self.seqs[ID_top]
+                    bottom_seq = self.seqs[ID_bottom]
+                    match_index = self.fragment_matcher(top_seq, bottom_seq)
+                    if match_index:
 
-                    x = 0
-                    x_stop = x + self.min_match_length
-                    while x_stop <= top_len:
-                        top_string = top_seq[x: x_stop]
-                        # print 'TOP'
-                        # print ''.join(top_string)
+                        map_top_bottom[ID_top] = {
+                            'ID_bottom_match' : ID_bottom,
+                            'match_index' : match_index
+                        }
 
-                        y = 0
-                        y_stop = y + self.min_match_length
-                        while y_stop <= bottom_len:
-                            bottom_string = bottom_seq[y: y_stop]
-                            # print ''.join(bottom_string)
+                        map_bottom_top[ID_bottom] = {
+                            'ID_top_match' : ID_top
+                        }
 
-                            if ''.join(top_string) == ''.join(bottom_string):
-                                # print '[x: x_stop]', x, x_stop
-                                # print '[y: y_stop]', y, y_stop
-                                top_seq_dict[top_index] = {
-                                    Assembler.MATCHING_BOTTOM_KEY: bottom_index
-                                }
+        # print 'TOP_BOTTOM'
+        # for k,v in map_top_bottom.iteritems():
+        #     print k,v
+        #
+        # print 'BOTTOM_TOP'
+        # for k,v in map_bottom_top.iteritems():
+        #     print k,v
 
-                                bottom_seq_dict[bottom_index] = {
-                                    Assembler.MATCHING_TOP_KEY: top_index
-                                }
-
-                                top_ranges_dict[top_index] = (x, x_stop)
-                                bottom_ranges_dict[bottom_index] = (y, y_stop)
-                                #TODO: account for almost identical sequences
-                                #TODO: FIGURE OUT WHEN TO BREAK
-
-                            y +=1
-                            y_stop +=1
-
-                        x+=1
-                        x_stop +=1
+        return map_top_bottom, map_bottom_top
 
 
-        return top_seq_dict, bottom_seq_dict, top_ranges_dict, bottom_ranges_dict
-
-
-    def _determine_order(self, top_seq_dict, bottom_seq_dict, top_ranges_dict, bottom_ranges_dict):
+    def _determine_order(self, map_top_bottom, map_bottom_top):
         '''
         determines the order of the sequences using the matching pairs info
 
         generates the following variables:
             order
-
         '''
 
-        # print 'top_seq_dict', top_seq_dict
-        # print "bottom_seq_dict", bottom_seq_dict
-        # print "top_ranges_dict", top_ranges_dict
-        # print "bottom_ranges_dict", bottom_ranges_dict
-        #start with one pair
-        first_seq_id = 0
-        if first_seq_id in bottom_seq_dict:
-            first_match = bottom_seq_dict[first_seq_id]
-            order= [first_match[Assembler.MATCHING_TOP_KEY], first_seq_id]
-        elif first_seq_id in top_seq_dict:
-            first_match = top_seq_dict[first_seq_id]
-            order = [first_seq_id, first_match[Assembler.MATCHING_BOTTOM_KEY]]
+        order = []
 
-        while len(order) != len(self.sequences):
+        first_seq_id = random.choice(self.seqs.keys())
+        print 'first_seq_id: ', first_seq_id
+        if first_seq_id in map_bottom_top:
+            order= [ map_bottom_top[first_seq_id]['ID_top_match'], first_seq_id ]
+        else:
+            order = [first_seq_id, map_top_bottom[first_seq_id]['ID_bottom_match']]
+
+        while len(order) < len(self.seqs): # is this one hacky?
             end = order[-1]
             start = order[0]
-            if end in top_seq_dict:
-                order.append(top_seq_dict[end][Assembler.MATCHING_BOTTOM_KEY])
-            elif start in bottom_seq_dict:
-                order.insert(0,bottom_seq_dict[start][Assembler.MATCHING_TOP_KEY])
+            if end in map_top_bottom:
+                order.append(map_top_bottom[end]['ID_bottom_match'])
+            elif start in map_bottom_top:
+                order.insert(0,map_bottom_top[start]['ID_top_match'])
 
         print 'order', order
         return order
+
 
     def assemble(self):
         '''
         assembles the sequence given the info on the pairs and the order
         '''
-        top_seq_dict, bottom_seq_dict, top_ranges_dict, bottom_ranges_dict = \
-            self._find_matching_pairs()
-        order = self._determine_order(top_seq_dict, bottom_seq_dict, top_ranges_dict, bottom_ranges_dict)
+        map_top_bottom, map_bottom_top = self._find_matching_pairs()
+        order = self._determine_order(map_top_bottom, map_bottom_top)
 
-        start_seq_index = order[0]
+        sequence = self.seqs[order[0]]
+        insert_index = 0
+        print ''.join(sequence)
+        for ID in order[0:-1]:
 
-        range_first_seq = top_ranges_dict[start_seq_index]
-        stop = range_first_seq[1]
-        self.sequence = self.seqs[start_seq_index][0:stop]
-        # import ipdb; ipdb.set_trace()
-
-        for seq_id in order[1:]:
-            start = bottom_ranges_dict[seq_id][1]
-
-            if seq_id == order[-1]:
-                segment_to_add = self.seqs[seq_id][start:]
-            else:
-                end = top_ranges_dict[seq_id][1]
-                segment_to_add = self.seqs[seq_id][start:end]
-
-            for b in segment_to_add:
-                self.sequence.append(b)
+            ID_bottom_match = map_top_bottom[ID]['ID_bottom_match']
+            match_index = map_top_bottom[ID]['match_index']
+            insert_index += match_index
+            print '----'
+            print ''.join(sequence[insert_index: insert_index + 5])
+            print ''.join(self.seqs[ID_bottom_match][0:5])
+            sequence[insert_index:] = self.seqs[ID_bottom_match]
+            # print '_'*insert_index + ''.join(self.seqs[ID_bottom_match])
 
         "Resulting assembled sequence"
-        print ''.join(self.sequence)
-        return ''.join(self.sequence)
+        print ''.join(sequence)
+        return ''.join(sequence)
